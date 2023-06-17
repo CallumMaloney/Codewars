a8cc927923389ed649b6ecde3a23734893d4c92f
CallumMaloney <callumfromanfield@hotmail.co.uk>
2023-06-17 23:25:20 +0100

    completed 7kyu kata exes and ohs

--- 7Kyu/Exes_and_Ohs.js
+++ 7Kyu/Exes_and_Ohs.js
@@ -15,21 +15,16 @@ XO("zzoo") => false
 
 
 /*
+PLAN-
 
+1. Declare 2 variables, each to hold the number of occurances of each letter
+2. Loop through the string, checking each letter
+3. If the letter is an "o" or "O", add 1 to the oList variable
+4. If the letter is an "x" or "X", add 1 to the xList variable
+5. Return true if the oList and xList variables are equal, false if not
 
 
 */
-
-
-
-
-
-
-
-
-
-
-
 
 function XO(str){
     let oList = 0
